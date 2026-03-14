c2c570a06e036a94ec8d03cd4f88b30933508c35
Guedes <sperex2012@gmail.com>
2026-03-14 03:49:37 +0000

    atualizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,16 +6,25 @@ int main() {
     char coordenadas_letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int coordenadas_numeros[1][10];
     int tabuleiro[10][10];
+    int navio_1[3];
+    int navio_2[3];
+
 
     printf (" ### BATALHA NAVAL ###\n\n");
     printf("   ");
 
-    for (int i = 0; i < 10; i++){
-        printf("%c ", coordenadas_letras[i]);
-    }
-    printf("\n");
 
-    for (int j = 0; j < 10; j++){
+    /*#############################
+      # APRESENTAÇÃO DO TABULEIRO #
+      #############################*/
+      // COORDENADAS SUPERIORES (LETRAS)
+     for (int i = 0; i < 10; i++){
+        printf("%c ", coordenadas_letras[i]);
+     }
+     printf("\n");
+
+     // COORDENADAS LATEAIS (NÚMEROS) + POSICIONAMENTO
+     for (int j = 0; j < 10; j++){
       if (j < 9){
          int a = (j + 1);
          coordenadas_numeros[0][j] = a; 
@@ -26,8 +35,8 @@ int main() {
              printf("%d ", tabuleiro[0][k]);
             }
          printf("\n");
-        } else {
-            int a = (j + 1);
+        } else { // Exceção à 10ª linha para organização do espaçamento da tabela
+         int a = (j + 1);
          coordenadas_numeros[0][j] = a; 
          printf("%d ",coordenadas_numeros[0][j]); 
 
@@ -35,7 +44,7 @@ int main() {
              tabuleiro[j][k] = 0;
              printf("%d ", tabuleiro[0][k]);
             }
-         printf("\n");
+         printf("\n\n");
         }
     }
                 
