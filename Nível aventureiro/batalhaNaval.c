d2867767fa3d03917ddaebc8a5633c5bd1054ded
Guedes <sperex2012@gmail.com>
2026-03-15 23:48:32 +0000

    finalizado

--- Nível aventureiro/batalhaNaval.c
+++ Nível aventureiro/batalhaNaval.c
@@ -13,6 +13,10 @@ int main() {
     int tabuleiro[horizontal][vertical];
     int navio_1[3] = {3, 3, 3};
     int navio_2[3] = {3, 3, 3};
+    int navio_3[3] = {3, 3, 3};
+    int navio_4[3] = {3, 3, 3};
+
+    
     
     
 
@@ -27,31 +31,37 @@ int main() {
       // *N* para números e *L* para letras
      for (int N = 0; N < vertical; N++){
         for (int L = 0; L < horizontal; L++){
-            tabuleiro[N][L] = 0; // Preenchimento do tabuleiro com 0
+            tabuleiro[L][N] = 0; // Preenchimento do tabuleiro com 0
+            // Declaração de coordenadas dos navios DIAGONAL
+            if((L == N && N > 0 && N < 4) ){ 
+             tabuleiro[L][N] = navio_1[N - 1];
+            } else if(((L + N) == 9 && N > 6)){
+             tabuleiro[L][N] = navio_2[N - 7];
+            } 
+            // Declaração de coordenadas dos navios VERTICAL/HORIZONTAL
+            else if((L == 6 && N > 2 && N < 6)){ 
+             tabuleiro[L][N] = navio_3[N - 3];
+            } else if((N == 4 && L > 1 && L < 5)){
+             tabuleiro[L][N] = navio_4[L - 2];
+            }
         }
      }
-            // Declaração de coordenadas dos navios  
-            /* (C5, D5, D5) e (G4, G5, G6) 
+        
+            /* (C5, D5, D5), (G4, G5, G6), (B2, C3, D4) e (A10, B9, C8) 
               A B C D E F G H I J 
             1 0 0 0 0 0 0 0 0 0 0 
-            2 0 0 0 0 0 0 0 0 0 0 
-            3 0 0 0 0 0 0 0 0 0 0 
-            4 0 0 0 0 0 0 3 0 0 0 
+            2 0 3 0 0 0 0 0 0 0 0 
+            3 0 0 3 0 0 0 0 0 0 0 
+            4 0 0 0 3 0 0 3 0 0 0 
             5 0 0 3 3 3 0 3 0 0 0 
             6 0 0 0 0 0 0 3 0 0 0 
             7 0 0 0 0 0 0 0 0 0 0 
-            8 0 0 0 0 0 0 0 0 0 0 
-            9 0 0 0 0 0 0 0 0 0 0 
-           10 0 0 0 0 0 0 0 0 0 0*/
+            8 0 0 3 0 0 0 0 0 0 0 
+            9 0 3 0 0 0 0 0 0 0 0 
+           10 3 0 0 0 0 0 0 0 0 0*/
 
            
-                tabuleiro[4][2] = navio_1[0];
-                tabuleiro[4][3] = navio_1[1];
-                tabuleiro[4][4] = navio_1[2];
-           
-                tabuleiro[3][6] = navio_2[0];
-                tabuleiro[4][6] = navio_2[1];
-                tabuleiro[5][6] = navio_2[2];
+       
 
     /*#############################
       # APRESENTAÇÃO DO TABULEIRO #
@@ -68,7 +78,7 @@ int main() {
          printf("%2d ",coordenadas_numeros[j]);
          /* Aumento da casa decimal para organização do espaçamento da tabela */  
         for(int k = 0; k < 10; k++){
-             printf("%d ", tabuleiro[j][k]);
+             printf("%d ", tabuleiro[k][j]);
              /* Apresentação da tabela com os valores dos navios */  
             }
          printf("\n");
